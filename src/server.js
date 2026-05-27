1be4dd1d015678af42bb658664bdd53bf94969c6
Alexander Phan <quannnd2004@gmail.com>
2026-05-27 15:41:09 +0700

    fix: correct typo in express import and add initial db configuration file

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const expoess = require('express');
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -6,4 +6,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express)
+
+app.get("/", (req,res) => {
+    res.json({
+        message: "Backend API running",
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
