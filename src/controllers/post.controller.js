bdb7346754c38f9c54c7fac58b48db6e1c7cf4b6
Alexander Phan <quannnd2004@gmail.com>
2026-05-28 12:52:17 +0700

    feat: add post model and controller with CRUD operations

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,34 @@
+const Post = require('../models/post.model');
+
+const getPosts = async (req, res) => {
+
+}
+
+const getPostById = async (req, res) => {
+
+}
+
+const createPost = async (req, res) => {
+
+}
+
+const updatePost = async (req, res) => {
+
+}
+
+const deletePost = async (req, res) => {
+
+}
+
+const toggleLikePost = async (req, res) => {
+
+}
+
+module.exports = {
+    getPosts,
+    getPostById,
+    createPost,
+    updatePost,
+    deletePost,
+    toggleLikePost,
+};
---
+++ src/models/post.model.js
@@ -0,0 +1,45 @@
+const mongooose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            trim: true,
+            minlength: [3, 'Title must be at least 3 characters long'],
+            maxlength: [100, 'Title must not exceed 100 characters'],
+        },
+
+        content: {
+            type: String,
+            required: [true, 'Content is required'],
+            trim: true,
+            minlength: [10, 'Content must be at least 10 characters long'],
+        },
+
+        category: {
+            type: String,
+            required: [true, 'Category is required'],
+            trim: true,
+            default: "General",
+        },
+
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('Post', postSchema);
